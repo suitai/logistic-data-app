48f1726d5338c9f52f9ca57cb9256e59b6988902
suitai <suitai@outlook.jp>
2016-06-19 22:25:28 +0900

    関数を分割

--- personal.py
+++ personal.py
@@ -9,26 +9,35 @@ def get_requests(payload):
     print "get:", payload
     return requests.get("https://api.frameworxopendata.jp/api/v3/datapoints", params=payload)
 
+
+def get_first_in_interval(requests, category, interval=10):
+    times = [""]    # ダミー
+    values = []
+
+    for d in requests.json():
+        date = dateutil.parser.parse(d['dc:date'])
+        time = str(date.hour).zfill(2) + ":" + str((date.minute//interval)*interval).zfill(2)
+        if time != times[-1]:
+            times.append(time)
+            values.append(d["frameworx:" + category])
+    del times[0]    # ダミーを削除
+
+    return times, values
+
+
 def get_chart_data(workerId, category):
     type_list = {'step': "frameworx:WarehouseVital",
                  'calorie': "frameworx:WarehouseVital",
                  'heartrate': "frameworx:WarehouseVital"}
     payload = {'rdf:type': type_list[category],
                'frameworx:workerId': workerId}
-    times = [""]    # ダミー
-    values = []
+    requests = get_requests(payload)
 
-    for d in get_requests(payload).json():
-        date = dateutil.parser.parse(d['dc:date'])
-        time = str(date.hour).zfill(2) + ":" + str((date.minute//10)*10).zfill(2)
-        if time != times[-1]:
-            times.append(time)
-            values.append(d["frameworx:" + category])
-    del times[0]    # ダミーを削除
+    value_x, value_y = get_first_in_interval(requests, category)
 
     data = {'label': category,
-            'value_x': times,
-            'value_y': values}
+            'value_x': value_x,
+            'value_y': value_y}
 
     return data
 
